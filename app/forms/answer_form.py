08eb40d3dd6156584df56a41ea8f2830972f7a4c
TonyTan <tonytan2193@gmail.com>
2022-11-02 18:38:21 -0700

    Finished editquestionform and added an editasnwerform

--- app/forms/answer_form.py
+++ app/forms/answer_form.py
@@ -1,8 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import Stringfield, Integerfield
+from wtforms import Stringfield, Integerfield, DateField
 from wtforms.validators import DataRequired
 
 class AnswerForm(FlaskForm):
     userId = Integerfield('userId', validators=[DataRequired()])
     questionId = Integerfield('questionId', validators=[DataRequired()])
     answer = Stringfield('Answer', validators=[DataRequired()])
+
+class EditAnswerForm(FlaskForm):
+    userId = Integerfield('userId', validators=[DataRequired()])
+    questionId = Integerfield('questionId', validators=[DataRequired()])
+    answer = Stringfield('Answer', validators=[DataRequired()])
+    updatedAt = DateField('Date', validators=[DataRequired()])
--- app/forms/question_form.py
+++ app/forms/question_form.py
@@ -1,7 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import Stringfield, Integerfield
+from wtforms import Stringfield, Integerfield, DateField
 from wtforms.validators import DataRequired
 
 class QuestionForm(FlaskForm):
     userId = Integerfield('userId', validators=[DataRequired()])
     question = Stringfield('Question', validators=[DataRequired()])
+
+class EditQuestionForm(FlaskForm):
+    userId = Integerfield('userId', validators=[DataRequired()])
+    question = Stringfield('Question', validators=[DataRequired()])
+    updatedAt = DateField('Date', validators=[DataRequired()])
