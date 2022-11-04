60c7bee042c118a0d2295c0c8b5a536c434f4daf
TonyTan <tonytan2193@gmail.com>
2022-11-04 14:37:48 -0700

    Added logos to nav bar and title

--- react-app/src/components/navbar/NavBar.js
+++ react-app/src/components/navbar/NavBar.js
@@ -2,13 +2,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import ProfileButton from './ProfileButton';
+import logo from './logo/answered-logo.png'
 import './NavBar.css'
 
 const NavBar = ({ user }) => {
   return (
     <nav id='navBar'>
       <NavLink to='/' exact={true} activeClassName='active'>
-        Home
+        <img className='logo' src={logo} />
       </NavLink>
       <ProfileButton user={user} />
     </nav>
