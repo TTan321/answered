0749bbcbb134ea28198a0d137f672c11852340b4
TonyTan <tonytan2193@gmail.com>
2022-11-03 19:01:34 -0700

    Created add an answer action and thunk and added to reducer

--- react-app/src/store/answers.js
+++ react-app/src/store/answers.js
@@ -1,11 +1,19 @@
 // Types
 const GET_ANSWERS = "answers/GET_ANSWERS"
+const ADD_ANSWER = "answers/ADD_ANSWER"
 
 // Actions
 const loadAnswers = answers => {
     return {
         type: GET_ANSWERS,
         answers
+    }
+}
+
+const postAnswer = answer => {
+    return {
+        type: ADD_ANSWER,
+        answer
     }
 }
 
@@ -19,14 +27,33 @@ export const getAnswers = () => async dispatch => {
     }
 }
 
+export const addAnswer = answer => async dispatch => {
+    const reponse = await fetch(`/api/answers`, {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(answer)
+    });
+    if (reponse.ok) {
+        const data = await reponse.json()
+        dispatch(postAnswer(data.answer))
+        return { ...data }
+    }
+}
+
 // Reducer
 const answersReducer = (state = {}, action) => {
     switch (action.type) {
         case GET_ANSWERS: {
             const newState = {}
             action.answers.forEach(answer => {
-                newState.answer.id = answer
+                newState[answer.id] = answer
             })
+            return newState
+        }
+        case ADD_ANSWER: {
+            const newAnswer = {}
+            newState[action.answer.id] = action.answer
+            const newState = { ...state, ...newAnswer }
             return newState
         }
         default: {
