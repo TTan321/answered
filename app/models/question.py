bd9288aa6ff8b8b3713399be756a9f1f629407b9
TonyTan <tonytan2193@gmail.com>
2022-11-07 13:51:00 -0800

    Added production and schema for deploying to render.com, removed all console logs

--- app/models/question.py
+++ app/models/question.py
@@ -1,11 +1,14 @@
-from .db import db
+from .db import db, environment, SCHEMA, add_prefix_for_prod
 from datetime import date
 
 class Question(db.Model):
     __tablename__ = "questions"
 
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')))
     question = db.Column(db.String(1000), nullable=False)
     created_at = db.Column(db.Date, default=date.today())
     updated_at = db.Column(db.Date, default=date.today())
