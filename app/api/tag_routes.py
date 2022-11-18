ca53d0fe5d62fef95faa4d0174aa8fc4217a0977
TonyTan <tonytan2193@gmail.com>
2022-11-17 18:35:31 -0800

    Made edit tag route

--- app/api/tag_routes.py
+++ app/api/tag_routes.py
@@ -31,3 +31,16 @@ def add_tag():
             db.session.commit()
             return {'tag': data.to_dict_tag_rel()}
     return form.errors
+
+# Edit a tag
+@tag_routes.route('/<int:tag_id>', methods=['PUT'])
+def edit_tag(tag_id):
+    form = TagForm()
+    tag = Tag.query.get(tag_id)
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        tag.name = form.data['name']
+        tag.image_url = form.data['image_url']
+        db.session.commit()
+        return {'tag': tag.to_dict_tag_rel()}
+    return form.errors
