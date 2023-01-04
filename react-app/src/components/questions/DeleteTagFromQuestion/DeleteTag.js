bcd993cca6ed0e39c0ecd71a37e517ddaeef5df7
TonyTan <tonytan2193@gmail.com>
2023-01-04 02:00:48 -0800

    Styled remove a tag alert

--- react-app/src/components/questions/DeleteTagFromQuestion/DeleteTag.js
+++ react-app/src/components/questions/DeleteTagFromQuestion/DeleteTag.js
@@ -1,6 +1,6 @@
 import { useDispatch } from "react-redux"
 import { loadQuestions, removeTagOnQuestion } from "../../../store/questions"
-
+import './DeleteTag.css'
 
 function DeleteTag({ setShowModal, tag, question }) {
     const dispatch = useDispatch();
@@ -23,13 +23,17 @@ function DeleteTag({ setShowModal, tag, question }) {
     }
 
     return (
-        <div>
-            <div>
-                <p>Do want to remove this tag from this question?</p>
-                <div>
-                    <button onClick={removeTag}>Remove</button>
-                    <button onClick={() => setShowModal(false)}>Cancel</button>
-                </div>
+        <div className="DeleteTag">
+            <div id='add-tag-cancel-div' onClick={() => setShowModal(false)}>
+                <i className="fas fa-times" />
+            </div>
+            <div className="DeleteAlert">
+                Alert
+            </div>
+            <p style={{ padding: '0 10px', fontWeight: 'bold' }}>Do want to remove this tag from this question?</p>
+            <div className="DeleteTagButtonDiv">
+                <button className="RemoveTagButton" onClick={removeTag}>Remove</button>
+                {/* <button onClick={() => setShowModal(false)}>Cancel</button> */}
             </div>
         </div>
     )
